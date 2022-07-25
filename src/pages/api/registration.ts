1f73f742823dc4b28aaccee388157bdfce83399f
Matt Colella <matt.colella@mongodb.com>
2022-07-25 14:25:41 -0400

    Add rate limiter to registration API endpoint

--- src/pages/api/registration.ts
+++ src/pages/api/registration.ts
@@ -4,6 +4,13 @@ import { Registration, validateRegistrationBody } from 'src/lib/registration';
 import { uploadToAirtable } from 'src/lib/airtable';
 import { uploadToAppServices } from 'src/lib/app-services';
 
+import rateLimit, { getIP, MAX_POSTS_PER_PERIOD } from 'src/lib/rate-limit';
+
+const limiter = rateLimit({
+  max: 600, // cache limit of 600 per 30 second period.
+  ttl: 30 * 1000,
+});
+
 const registrationHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
@@ -11,6 +18,14 @@ const registrationHandler = async (
   if (req.method !== 'POST') {
     return res.status(405).send('This is a POST-only endpoint.');
   }
+
+  try {
+    const IP = getIP(req);
+    await limiter.check(res, MAX_POSTS_PER_PERIOD, IP || '');
+  } catch {
+    return res.status(500).json({ error: { message: 'Something went wrong' } });
+  }
+
   try {
     validateRegistrationBody(req.body);
   } catch (err: any) {
