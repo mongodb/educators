51ce1d285c96f1af5deabfa61d60df58ca70c2d7
Matt Colella <matt.colella@mongodb.com>
2022-07-25 11:02:47 -0400

    format

--- src/pages/api/registration.ts
+++ src/pages/api/registration.ts
@@ -4,16 +4,16 @@ import { Registration, validateRegistrationBody } from 'src/lib/registration';
 import { uploadToAirtable } from 'src/lib/airtable';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method !== 'POST') {
-		return res.status(405).send('This is a POST-only endpoint.');
-	}
-	try {
-		validateRegistrationBody(req.body);
-	} catch (err: any) {
-		return res.status(400).send(err.message);
-	}
+  if (req.method !== 'POST') {
+    return res.status(405).send('This is a POST-only endpoint.');
+  }
+  try {
+    validateRegistrationBody(req.body);
+  } catch (err: any) {
+    return res.status(400).send(err.message);
+  }
 
-	await uploadToAirtable(req.body as Registration);
+  await uploadToAirtable(req.body as Registration);
 
-	return res.status(200).json({});
+  return res.status(200).json({});
 };
