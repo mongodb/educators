97305f852c064f20be6a26715f3a3695611d9a85
Matt Colella <matt.colella@mongodb.com>
2022-08-01 13:19:04 -0400

    Fix course-body test

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,44 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/', 'src'],
   testEnvironment: 'jest-environment-jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     lib$: '<rootDir>/src/lib/$1',
     'pages/(.*)$': '<rootDir>/src/pages/$1',
     'styles/(.*)$': '<rootDir>/src/styles/$1',
     'components/(.*)$': '<rootDir>/src/components/$1',
+    'react-markdown':
+      '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
   },
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = async () => {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  const transformModules = [
+    'remark-directive',
+    'unist-util-visit',
+    'unist-util-visit-parents',
+    'unist-util-is',
+    'micromark-extension-directive',
+    'micromark-factory-space',
+    'micromark-util-character',
+    'micromark-factory-whitespace',
+    'parse-entities',
+    'character-entities-legacy',
+    'character-reference-invalid',
+    'is-decimal',
+    'is-hexadecimal',
+    'is-alphanumerical',
+    'is-alphabetical',
+    'decode-named-character-reference',
+    'mdast-util-directive',
+    'stringify-entities',
+    'character-entities-html4',
+    'mdast-util-to-markdown',
+  ];
+
+  nextJestConfig.transformIgnorePatterns[0] = `/node_modules/(?!(${transformModules.join(
+    '|'
+  )})/)`;
+  return nextJestConfig;
+};
