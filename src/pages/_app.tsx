84971eebc1028057ef78d02fa6fe576860c89503
Justin McKenzie <justin.mckenzie@mongodb.com>
2022-08-18 14:19:12 -0400

    add OG metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,29 @@ export default function EducatorPortal({ Component, pageProps }: AppProps) {
       <Head>
         <title>MongoDB Educator Center</title>
         <link rel="icon" href="/academia/favicon.ico" />
+        {/* Open Graph Default Metadata (Used by Facebook, LinkedIn) */}
+        <meta property="og:title" content="MongoDB Educator Center" />
+        <meta
+          property="og:description"
+          content="Explore free resources for educators crafted by MongoDB experts to prepare learners with in-demand database skills and knowledge."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.mongodb.com/academia" />
+        <meta
+          property="og:image"
+          content="https://www.mongodb.com/academia/desk.svg"
+        />
+        <meta property="og:image:alt" content="MongoDB Educator Center" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="640" />
+
+        {/* Open Graph Default Metadata For Twitter */}
+        <meta property="twitter:card" content="MongoDB Educator Center" />
+        <meta
+          property="twitter:image"
+          content="https://www.mongodb.com/academia/desk.svg"
+        />
+        <meta property="twitter:site" content="@MongoDB" />
       </Head>
       <ThemeProvider theme={theme}>
         <Layout isFormOpen={isFormOpen}>
