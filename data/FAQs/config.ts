7d426b68610944445dc5c2c093b92f80cc488fc0
Justin McKenzie <justin.mckenzie@mongodb.com>
2023-02-07 09:24:48 -0500

    [LRN-341]: Frontend - create UI for FAQ section (#94)
    * add accordion component and markdown for FAQS
    
    * add test case
    
    * code cleanup

---
+++ data/FAQs/config.ts
@@ -0,0 +1,54 @@
+import eligibilityFAQ from './faq-eligibility.md';
+import applicationProcessFAQ from './faq-application-process.md';
+import promotionalCodeFAQ from './faq-promotional-code.md';
+import creditsFAQ from './faq-credits.md';
+import studentBenefitsFAQ from './faq-student-benefits.md';
+import certficationFAQ from './faq-certfication.md';
+import contactFAQ from './faq-contact.md';
+
+const CONFIG = [
+  {
+    title: 'Who is eligible for the MongoDB for Educators Program?',
+    body: eligibilityFAQ as unknown as string, // lets us typecast the markdown as string since it comes in as "(MDXProps) => Element"
+  },
+  {
+    title:
+      'Can educators outside of the United States apply for the MongoDB for Educator Program?',
+    body: 'Yes!',
+  },
+  {
+    title: 'How does the application process work?',
+    body: applicationProcessFAQ as unknown as string,
+  },
+  {
+    title:
+      'What is the expected turnaround time to be verified as an educator?',
+    body: 'Applications that are not instantly approved will be reviewed and addressed within 5-7 days.',
+  },
+  {
+    title: 'How will I receive my Atlas Promotional Code?',
+    body: promotionalCodeFAQ as unknown as string,
+  },
+  {
+    title: 'How do MongoDB Atlas credits work?',
+    body: creditsFAQ as unknown as string,
+  },
+  {
+    title: 'Do MongoDB Atlas Promotional Codes expire?',
+    body: "Atlas Promotional Codes will expire if they're not applied to your Atlas account within 90 days. Once applied, you will have 12 months to use all of the credits.",
+  },
+  {
+    title: 'What benefits are available for my students?',
+    body: studentBenefitsFAQ as unknown as string,
+  },
+  {
+    title: 'How do I access free certification for educators?',
+    body: certficationFAQ as unknown as string,
+  },
+  {
+    title: 'I have more questions, who can I contact?',
+    body: contactFAQ as unknown as string,
+  },
+];
+
+export default CONFIG;
