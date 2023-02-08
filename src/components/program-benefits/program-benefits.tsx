d57d70e1c02d8ffdffd6c04f151a45cfaabafce0
Justin McKenzie <justin.mckenzie@mongodb.com>
2023-02-08 13:41:56 -0500

    update benefits styling

--- src/components/program-benefits/program-benefits.tsx
+++ src/components/program-benefits/program-benefits.tsx
@@ -15,67 +15,72 @@ export default function ProgramBenefits() {
 
   return (
     <section sx={styles.ProgramBenefitsWrapper}>
-      <GridLayout sx={{ rowGap: 0 }}>
-        <TypographyScale variant="heading3" sx={styles.ProgramBenefitsTitle}>
-          MongoDB for Educators Program Benefits
-        </TypographyScale>
-        <Grid columns={[1, null, 3]} sx={styles.ProgramBenefitsWidgetsWrapper}>
-          <div sx={styles.ProgramBenefitsWidget}>
-            <Image
-              src="/academia/atlas-credits.svg"
-              alt="MongoDB corporate icon"
-            />
-            <TypographyScale
-              variant="heading6"
-              sx={styles.ProgramBenefitsWidgetTitle}
-            >
-              Get $500 of Atlas Credits
-            </TypographyScale>
-            <TypographyScale
-              variant="body2"
-              sx={styles.ProgramBenefitsWidgetSubtitle}
-            >
-              Explore our multi-cloud developer platform with $500 of MongoDB
-              Atlas credits.
-            </TypographyScale>
-          </div>
-          <div sx={styles.ProgramBenefitsWidget}>
-            <Image
-              src="/academia/certifications.svg"
-              alt="MongoDB corporate icon"
-            />
-            <TypographyScale
-              variant="heading6"
-              sx={styles.ProgramBenefitsWidgetTitle}
-            >
-              Free Certifications
-            </TypographyScale>
-            <TypographyScale
-              variant="body2"
-              sx={styles.ProgramBenefitsWidgetSubtitle}
-            >
-              Through our educator program, you will be eligible for free
-              MongoDB certification.
-            </TypographyScale>
-          </div>
-          <div sx={styles.ProgramBenefitsWidget}>
-            <Image src="/academia/community.svg" alt="MongoDB corporate icon" />
-            <TypographyScale
-              variant="heading6"
-              sx={styles.ProgramBenefitsWidgetTitle}
-            >
-              Community
-            </TypographyScale>
-            <TypographyScale
-              variant="body2"
-              sx={styles.ProgramBenefitsWidgetSubtitle}
-            >
-              Connect with our global educator community through our educator
-              forum and exclusive events for educators.
-            </TypographyScale>
-          </div>
-        </Grid>
-      </GridLayout>
+      <TypographyScale variant="heading3" sx={styles.ProgramBenefitsTitle}>
+        MongoDB for Educators Program Benefits
+      </TypographyScale>
+      <div sx={styles.ProgramBenefitsWidgetWrapper}>
+        <GridLayout sx={{ rowGap: 0 }}>
+          <Grid columns={[1, null, 3]} sx={styles.ProgramBenefitsGrid}>
+            <div sx={styles.ProgramBenefitsWidget}>
+              <Image
+                src="/academia/atlas-credits.svg"
+                alt="MongoDB corporate icon"
+              />
+              <TypographyScale
+                variant="heading6"
+                sx={styles.ProgramBenefitsWidgetTitle}
+              >
+                Get $500 of Atlas Credits
+              </TypographyScale>
+              <TypographyScale
+                variant="body2"
+                sx={styles.ProgramBenefitsWidgetSubtitle}
+              >
+                Explore our multi-cloud developer platform with $500 of MongoDB
+                Atlas credits.
+              </TypographyScale>
+            </div>
+            <div sx={styles.ProgramBenefitsWidget}>
+              <Image
+                src="/academia/certifications.svg"
+                alt="MongoDB corporate icon"
+              />
+              <TypographyScale
+                variant="heading6"
+                sx={styles.ProgramBenefitsWidgetTitle}
+              >
+                Free Certifications
+              </TypographyScale>
+              <TypographyScale
+                variant="body2"
+                sx={styles.ProgramBenefitsWidgetSubtitle}
+              >
+                Through our educator program, you will be eligible for free
+                MongoDB certification.
+              </TypographyScale>
+            </div>
+            <div sx={styles.ProgramBenefitsWidget}>
+              <Image
+                src="/academia/community.svg"
+                alt="MongoDB corporate icon"
+              />
+              <TypographyScale
+                variant="heading6"
+                sx={styles.ProgramBenefitsWidgetTitle}
+              >
+                Community
+              </TypographyScale>
+              <TypographyScale
+                variant="body2"
+                sx={styles.ProgramBenefitsWidgetSubtitle}
+              >
+                Connect with our global educator community through our educator
+                forum and exclusive events for educators.
+              </TypographyScale>
+            </div>
+          </Grid>
+        </GridLayout>
+      </div>
       <Link
         sx={styles.ProgramBenefitsCTA}
         linkIcon="arrow"
