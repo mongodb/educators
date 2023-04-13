8ac96fd8fcaad00b961b725d213c5956bb63f2e8
Justin McKenzie <justin.mckenzie@mongodb.com>
2023-04-13 11:14:23 -0400

    self review

--- src/styles/phd-fellowship.ts
+++ src/styles/phd-fellowship.ts
@@ -14,7 +14,7 @@ const HeroWrapper: ThemeUICSSObject = {
   h2: {
     textAlign: 'center',
     paddingBottom: ['inc70', null, 'inc50', 'inc40'],
-  }
+  },
 };
 
 const Main = {
@@ -25,20 +25,20 @@ const Main = {
 
 const Section = {
   marginTop: ['inc70', null, null, 'inc60'],
-  marginBottom: ['inc80', null, null, 'inc90']
-}
+  marginBottom: ['inc80', null, null, 'inc90'],
+};
 
 const InfoSection = {
   marginBottom: ['inc40', null, null, 'inc50'],
   '&:last-of-type': {
     marginBottom: 0,
-  }
+  },
 };
 
 const InfoSectionText = {
   display: 'block',
   paddingTop: ['inc30', null, 'inc40'],
-  paddingBottom: 'inc40'
+  paddingBottom: 'inc40',
 };
 
 const OrderedList = {
@@ -55,7 +55,7 @@ const TextBtn = {
   '&:hover': {
     cursor: 'pointer',
     borderBottom: `2px solid ${theme.colors.black80}`,
-  }
+  },
 };
 
 const EmailLink = {
@@ -63,7 +63,7 @@ const EmailLink = {
   '&:hover': {
     cursor: 'pointer',
     borderBottom: `2px solid ${theme.colors.black80}`,
-  }
+  },
 };
 
 const FAQTitle: ThemeUICSSObject = {
@@ -74,9 +74,9 @@ const FAQTitle: ThemeUICSSObject = {
 const FAQStyles = {
   ul: {
     marginTop: ['inc30', null, 'inc40'],
-    marginBottom: 0
-  }
-}
+    marginBottom: 0,
+  },
+};
 
 const styles = {
   Main,
