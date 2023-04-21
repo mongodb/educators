49d3d09e2d3d5892bd0eb9bf829f7807ebcfe0a7
Justin McKenzie <justin.mckenzie@mongodb.com>
2023-04-21 13:55:52 -0400

    test

--- src/components/form/form.spec.tsx
+++ src/components/form/form.spec.tsx
@@ -1,12 +1,15 @@
-// TODO: Revisting once integration and testing is complete
-
 // import axios from 'axios';
 // import { act, render, screen, fireEvent } from '@testing-library/react';
 // import Form, { isRequired, emailPattern } from '.';
 
-// jest.mock('axios');
+import Form from '.';
+import { render, screen } from '@testing-library/react';
 
-// // Because of validators on form fields, we need to populate all of the "required" fields in order to test submitting the form
+jest.mock('axios');
+
+// TODO: Tests will be cleaned up once testing and integration is complete
+
+// Because of validators on form fields, we need to populate all of the "required" fields in order to test submitting the form
 // function populateRequiredFormFields(
 //   inputs: HTMLCollectionOf<HTMLInputElement>,
 //   selects: HTMLElement[]
@@ -42,129 +45,142 @@
 //   fireEvent.blur(webUrlInput);
 // }
 
-// describe('[component] Form', () => {
-//   it('renders the component', () => {
-//     render(<Form isOpen closeForm={() => {}} />);
+describe('[component] Form', () => {
+  it('renders the component', () => {
+    render(
+      <Form
+        isOpen
+        closeForm={() => {}}
+        texts={{
+          title: 'Test',
+          postSubmissionTitle: 'Test',
+          postSubmissionDescription: 'Test',
+          button: 'Submit',
+        }}
+        fields={[{ name: 'test', label: 'test' }]}
+        submitForm={() => {}}
+      />
+    );
 
-//     const form = screen.getByTestId('form-modal');
-//     expect(form).toBeInTheDocument();
-//   });
+    const form = screen.getByTestId('form-modal');
+    expect(form).toBeInTheDocument();
+  });
 
-//   it('does not render the component if isOpen prop is false', () => {
-//     const { container } = render(<Form isOpen={false} closeForm={() => {}} />);
-//     expect(container.firstChild).toBeNull();
-//   });
+  // it('does not render the component if isOpen prop is false', () => {
+  //   const { container } = render(<Form isOpen={false} closeForm={() => {}} />);
+  //   expect(container.firstChild).toBeNull();
+  // });
 
-//   it('validates input if field is required', () => {
-//     expect(isRequired()('hello world')).toEqual(''); // No warning is returned if text is entered
-//     expect(isRequired()('')).toEqual('This field is required');
-//   });
+  // it('validates input if field is required', () => {
+  //   expect(isRequired()('hello world')).toEqual(''); // No warning is returned if text is entered
+  //   expect(isRequired()('')).toEqual('This field is required');
+  // });
 
-//   it('validates if email input matches an email address pattern', () => {
-//     expect(emailPattern()('')).toEqual(''); // passes if no value provided
-//     expect(emailPattern()('firstlast@gmail')).toEqual(
-//       'Please enter a valid email address'
-//     );
-//     expect(emailPattern()('firstlast')).toEqual(
-//       'Please enter a valid email address'
-//     );
-//     expect(emailPattern()('firstlast@gmail.')).toEqual(
-//       'Please enter a valid email address'
-//     );
-//     expect(emailPattern()('firstlast@gmail.com')).toEqual(
-//       'School or Institution email is required'
-//     ); // email domain is in "excluded" (free) list
+  // it('validates if email input matches an email address pattern', () => {
+  //   expect(emailPattern()('')).toEqual(''); // passes if no value provided
+  //   expect(emailPattern()('firstlast@gmail')).toEqual(
+  //     'Please enter a valid email address'
+  //   );
+  //   expect(emailPattern()('firstlast')).toEqual(
+  //     'Please enter a valid email address'
+  //   );
+  //   expect(emailPattern()('firstlast@gmail.')).toEqual(
+  //     'Please enter a valid email address'
+  //   );
+  //   expect(emailPattern()('firstlast@gmail.com')).toEqual(
+  //     'School or Institution email is required'
+  //   ); // email domain is in "excluded" (free) list
 
-//     expect(emailPattern()('firstlast@myfakeschool.edu')).toEqual(''); // is valid email domain and not found in blacklisted free email domains
-//   });
+  //   expect(emailPattern()('firstlast@myfakeschool.edu')).toEqual(''); // is valid email domain and not found in blacklisted free email domains
+  // });
 
-//   it('renders the expected components', () => {
-//     const { container } = render(<Form isOpen closeForm={() => {}} />);
+  // it('renders the expected components', () => {
+  //   const { container } = render(<Form isOpen closeForm={() => {}} />);
 
-//     // Text inputs
-//     const inputs = container.getElementsByTagName('input');
-//     expect(inputs.length).toEqual(7); // 6 text inputs and 1 checkbox input
+  //   // Text inputs
+  //   const inputs = container.getElementsByTagName('input');
+  //   expect(inputs.length).toEqual(7); // 6 text inputs and 1 checkbox input
 
-//     // Selects
-//     const selects = screen.getAllByRole('select'); // Flora Dropdowns are buttons with role="select", not native <select /> elements
-//     expect(selects.length).toEqual(3);
+  //   // Selects
+  //   const selects = screen.getAllByRole('select'); // Flora Dropdowns are buttons with role="select", not native <select /> elements
+  //   expect(selects.length).toEqual(3);
 
-//     // Submit Button
-//     const submitBtn = container.querySelectorAll('[type="submit"]');
-//     expect(submitBtn.length).toEqual(1);
-//   });
+  //   // Submit Button
+  //   const submitBtn = container.querySelectorAll('[type="submit"]');
+  //   expect(submitBtn.length).toEqual(1);
+  // });
 
-//   it('calls the onClose prop when the close modal button is clicked', () => {
-//     const onClose = jest.fn();
-//     const { container } = render(<Form isOpen closeForm={onClose} />);
+  // it('calls the onClose prop when the close modal button is clicked', () => {
+  //   const onClose = jest.fn();
+  //   const { container } = render(<Form isOpen closeForm={onClose} />);
 
-//     const closeBtn = container.getElementsByTagName('button')[0]; // first button found in component is the close button
+  //   const closeBtn = container.getElementsByTagName('button')[0]; // first button found in component is the close button
 
-//     fireEvent.click(closeBtn);
+  //   fireEvent.click(closeBtn);
 
-//     expect(onClose).toHaveBeenCalledTimes(1);
-//   });
+  //   expect(onClose).toHaveBeenCalledTimes(1);
+  // });
 
-//   it('should display post submission message if submit POST is successful', async () => {
-//     const { container } = render(<Form isOpen closeForm={() => {}} />);
+  // it('should display post submission message if submit POST is successful', async () => {
+  //   const { container } = render(<Form isOpen closeForm={() => {}} />);
 
-//     const inputs = container.getElementsByTagName('input');
-//     const selects = screen.getAllByRole('select');
+  //   const inputs = container.getElementsByTagName('input');
+  //   const selects = screen.getAllByRole('select');
 
-//     populateRequiredFormFields(inputs, selects);
-//     populateCourseSyllabusField();
+  //   populateRequiredFormFields(inputs, selects);
+  //   populateCourseSyllabusField();
 
-//     await act(async () => {
-//       fireEvent.click(screen.getByText('Submit my Application'));
-//     });
+  //   await act(async () => {
+  //     fireEvent.click(screen.getByText('Submit my Application'));
+  //   });
 
-//     expect(
-//       screen.getByText('Thanks for applying to MongoDB for Educators!')
-//     ).toBeInTheDocument();
-//     expect(
-//       screen.getByText(
-//         'We will review your application and email you within 5-7 business days.'
-//       )
-//     ).toBeInTheDocument();
-//   });
+  //   expect(
+  //     screen.getByText('Thanks for applying to MongoDB for Educators!')
+  //   ).toBeInTheDocument();
+  //   expect(
+  //     screen.getByText(
+  //       'We will review your application and email you within 5-7 business days.'
+  //     )
+  //   ).toBeInTheDocument();
+  // });
 
-//   it('should display error message if submit POST fails', async () => {
-//     jest.spyOn(axios, 'post').mockRejectedValue(Promise.resolve(new Error()));
+  // it('should display error message if submit POST fails', async () => {
+  //   jest.spyOn(axios, 'post').mockRejectedValue(Promise.resolve(new Error()));
 
-//     const { container } = render(<Form isOpen closeForm={() => {}} />);
+  //   const { container } = render(<Form isOpen closeForm={() => {}} />);
 
-//     const inputs = container.getElementsByTagName('input');
-//     const selects = screen.getAllByRole('select');
+  //   const inputs = container.getElementsByTagName('input');
+  //   const selects = screen.getAllByRole('select');
 
-//     populateRequiredFormFields(inputs, selects);
-//     populateCourseSyllabusField();
+  //   populateRequiredFormFields(inputs, selects);
+  //   populateCourseSyllabusField();
 
-//     await act(async () => {
-//       fireEvent.click(screen.getByText('Submit my Application'));
-//     });
+  //   await act(async () => {
+  //     fireEvent.click(screen.getByText('Submit my Application'));
+  //   });
 
-//     expect(
-//       screen.getByText(
-//         'There was an error submitting your request. Please try again.'
-//       )
-//     ).toBeInTheDocument();
-//   });
+  //   expect(
+  //     screen.getByText(
+  //       'There was an error submitting your request. Please try again.'
+  //     )
+  //   ).toBeInTheDocument();
+  // });
 
-//   it('displays course syllabus field options', () => {
-//     render(<Form isOpen closeForm={() => {}} />);
+  // it('displays course syllabus field options', () => {
+  //   render(<Form isOpen closeForm={() => {}} />);
 
-//     const uploadDocumentBtn = screen.getByText('Upload a document');
-//     const enterWebUrlBtn = screen.getByText('Enter a Web URL');
+  //   const uploadDocumentBtn = screen.getByText('Upload a document');
+  //   const enterWebUrlBtn = screen.getByText('Enter a Web URL');
 
-//     expect(uploadDocumentBtn).toBeInTheDocument();
-//     expect(enterWebUrlBtn).toBeInTheDocument();
+  //   expect(uploadDocumentBtn).toBeInTheDocument();
+  //   expect(enterWebUrlBtn).toBeInTheDocument();
 
-//     fireEvent.click(enterWebUrlBtn);
+  //   fireEvent.click(enterWebUrlBtn);
 
-//     expect(screen.getByPlaceholderText('Enter Web URL')).toBeInTheDocument();
+  //   expect(screen.getByPlaceholderText('Enter Web URL')).toBeInTheDocument();
 
-//     fireEvent.click(uploadDocumentBtn);
+  //   fireEvent.click(uploadDocumentBtn);
 
-//     expect(screen.getByTestId('document-upload')).toBeInTheDocument();
-//   });
-// });
+  //   expect(screen.getByTestId('document-upload')).toBeInTheDocument();
+  // });
+});
